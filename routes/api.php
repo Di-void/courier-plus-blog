e75a9ea2a0385cc758bdce72c7aa3254c5e8bc4a
DEE <donzydonathan@gmail.com>
2024-09-26 17:59:09 +0100

    feat: complete db models and migrations

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('v1')->group(function () {
+    Route::get('/test', function () {
+        return "Hello world";
+    });
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
