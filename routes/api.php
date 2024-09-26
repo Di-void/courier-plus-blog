30f4abcfa1271db488e751aca1e4a3497cc87728
DEE <donzydonathan@gmail.com>
2024-09-26 19:58:54 +0100

    feat: add blog controller as a resource controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->middleware('courier.custom')->group(function () {
+    Route::resources([
+        'blogs' => BlogController::class,
+    ]);
+
     Route::get('/test', function () {
         return "Hello world";
     });
